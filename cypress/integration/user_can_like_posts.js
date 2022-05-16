214f4bc33f93c1bb24a31d401f64164dab1fe662
Jordan <jordanmanu@live.com.au>
2022-05-16 17:00:24 +0100

    added new posts to the posts page, fixed tests

--- cypress/integration/user_can_like_posts.js
+++ cypress/integration/user_can_like_posts.js
@@ -17,7 +17,6 @@ describe("Timeline", () => {
 
     // submit a post
     cy.visit("/posts");
-    cy.contains("New post").click();
 
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
     cy.get("#new-post-form").submit();
--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -17,7 +17,6 @@ describe("Timeline", () => {
 
     // submit a post
     cy.visit("/posts");
-    cy.contains("New post").click();
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
     cy.get("#new-post-form").submit();
     cy.get(".posts").should("contain", "Hello, world!");
@@ -25,14 +24,11 @@ describe("Timeline", () => {
 
     // checks if posts are in order of newest first
     cy.visit("/posts");
-    cy.contains("New post").click();
 
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world1!");
     cy.get("#new-post-form").submit();
-    cy.contains("New post").click();
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world2!");
     cy.get("#new-post-form").submit();
-    cy.contains("New post").click();
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world3!");
     cy.get("#new-post-form").submit();
 
