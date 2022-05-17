e22582331c743c11cc1b14e024b8f9521985b08e
Jordan <jordanmanu@live.com.au>
2022-05-17 11:00:15 +0100

    got user Id showing on posts

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema(
   {
     message: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     likes: { type: Number, default: 0 },
   },
   { timestamps: true }
